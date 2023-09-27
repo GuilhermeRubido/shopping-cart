9f71971f3b0acf9b59efcf4acd25effb3ed3df35
Guilherme Rubido <rubidoguilherme@gmail.com>
2023-09-27 16:15:35 -0300

    Funcionalidade do carrinho e finaliza o projeto!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 import Provider from './context/Provider';
+import Cart from './components/Cart/Cart';
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <Provider>
         <Header/>
         <Products/>
+        <Cart/>
       </Provider>
     );
   }
